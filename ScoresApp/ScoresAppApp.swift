fd7edfb578dc6971aec6ee55dd08651c4405eaf7
YerayCastro <112658475+YerayCastro@users.noreply.github.com>
2023-11-08 12:44:13 +0100

    Initial Commit

---
+++ ScoresApp/ScoresAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoresAppApp.swift
+//  ScoresApp
+//
+//  Created by Yery Castro on 8/11/23.
+//
+
+import SwiftUI
+
+@main
+struct ScoresAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
